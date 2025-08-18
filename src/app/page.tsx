a07cf2b02a1a56b450e0ffc9978486b1c0d2e7ea
helios9284 <colonelme9284@gmail.com>
2025-08-18 19:14:24 -0400

    bug fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -390,7 +390,7 @@ export default function Home() {
             onClick={handlePrev}
             disabled={activeIndex === 0}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-8 group-hover:stroke-[#A020F0] transition">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-8 group-hover:stroke-[#A020F0] transition">
               <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
             </svg>
           </button>
@@ -400,7 +400,7 @@ export default function Home() {
             onClick={handleNext}
             disabled={activeIndex === cardCount - 1}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="size-6 group-hover:stroke-[#A020F0] transition">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6 group-hover:stroke-[#A020F0] transition">
               <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" />
             </svg>
           </button>
